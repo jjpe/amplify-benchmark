bab36b30dd7299fb1b6cbbf096b7b54fd736a59e
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-08-04 14:44:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Amplify Benchmark
+
+An easy way to test Amplify-related code that also accounts for the time spent
+in Emacs itself.
+
+## Setup
+
+TODO
+
+## Usage
+
+TODO
---
+++ amplify-benchmark.el
@@ -0,0 +1,15 @@
+;;; amplify-benchmark -- a tool to benchmark my thesis project.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'depend   "./depend.el/depend.el")
+(require 'spoofax  "../spoofax-mode/spoofax.el")
+(require 'ert)
+
+
+
+
+(provide 'amplify-benchmark)
+;;; amplify-benchmark.el ends here
